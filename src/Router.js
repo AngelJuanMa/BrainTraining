22284f815cddb52d760ccba014aa8ffdb7840c6f
AngelJuanMa <angelcarballo66666@gmail.com>
2020-08-12 18:25:35 -0300

    start

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect, HashRouter } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import Footer from './components/footer/footer.component.jsx';
 import Login from './components/login/login.component.jsx';
-import Register from './components/register/register.component.jsx';
-import Main from './components/main/main.component.jsx';
-import redirect from './components/redirect/redirect.component.jsx';
 import { createBrowserHistory } from 'history';
 
 const customHistory = createBrowserHistory();
@@ -17,13 +13,10 @@ class Router extends Component {
             <BrowserRouter>
                 <HashRouter basename="/">
                     <Switch>
-                        <Route history={customHistory} exact path="/" component={Main} />
+                        <Route history={customHistory} exact path="/" component={Login} />
                         <Route history={customHistory} exact path="/login" component={Login} />
-                        <Route history={customHistory} exact path="/registro" component={Register} />
-                        <Route history={customHistory} exact path="" component={redirect} />
                     </Switch>
                 </HashRouter>
-                <Footer></Footer>
             </BrowserRouter>
 
         );
--- src/components/login/login.component.jsx
+++ src/components/login/login.component.jsx
@@ -1,45 +1,13 @@
 import React, { Component } from "react";
 import { NavLink, Redirect } from "react-router-dom";
-import Header from "../header/header.component.jsx";
 import "./login.style.sass";
 
 class Login extends Component {
-  state = {
-    redirect: false
-  }
-
-  email = React.createRef();
-  password = React.createRef();
-
-  signIn = (e) => {
-    e.preventDefault();
-    let user = {
-      email: this.email.current.value,
-      password: this.password.current.value
-    } 
-    localStorage.setItem('identity', JSON.stringify(user))
-    this.setState({ redirect: true })
-  }
-
   render() {
-    if (localStorage.getItem('identity')) this.setState({ redirect: true });
-    if (this.state.redirect) return <Redirect to='/' />
-
     return (
       <React.Fragment>
-        <Header></Header>
         <div id="loginComponent">
           <h1>¡Hola! Ingresá tu e‑mail o usuario</h1>
-          <form onSubmit={this.signIn}>
-            <input type="email" ref={this.email} placeholder="E-mail o usuario" />
-
-            <input type="password" ref={this.password} placeholder="Contraseña" name="password" />
-
-            <input type="submit" name="submit" />
-          </form>
-          <NavLink to="registro" id="createAccount">
-            Crear cuenta
-          </NavLink>
         </div>
       </React.Fragment>
     );
